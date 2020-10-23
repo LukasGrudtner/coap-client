a7152b6ff0eed35982575ef2392e05b6d3a065d9
Lukas Derner Grüdtner <lukasgrudtner@hotmail.com>
2020-10-23 15:09:11 -0300

    Add UART data handling

--- main/coap_client_example_main.c
+++ main/coap_client_example_main.c
@@ -95,6 +95,86 @@ extern uint8_t client_key_start[] asm("_binary_coap_client_key_start");
 extern uint8_t client_key_end[]   asm("_binary_coap_client_key_end");
 #endif /* CONFIG_COAP_MBEDTLS_PKI */
 
+#include "driver/uart.h"
+#include "driver/gpio.h"
+
+/**
+ * This is an example which echos any data it receives on configured UART back to the sender,
+ * with hardware flow control turned off. It does not use UART driver event queue.
+ *
+ * - Port: configured UART
+ * - Receive (Rx) buffer: on
+ * - Transmit (Tx) buffer: off
+ * - Flow control: off
+ * - Event queue: off
+ * - Pin assignment: see defines below (See Kconfig)
+ */
+
+#define ECHO_TEST_TXD (CONFIG_EXAMPLE_UART_TXD)
+#define ECHO_TEST_RXD (CONFIG_EXAMPLE_UART_RXD)
+#define ECHO_TEST_RTS (UART_PIN_NO_CHANGE)
+#define ECHO_TEST_CTS (UART_PIN_NO_CHANGE)
+
+#define ECHO_UART_PORT_NUM      (CONFIG_EXAMPLE_UART_PORT_NUM)
+#define ECHO_UART_BAUD_RATE     (CONFIG_EXAMPLE_UART_BAUD_RATE)
+#define ECHO_TASK_STACK_SIZE    (CONFIG_EXAMPLE_TASK_STACK_SIZE)
+
+#define BUF_SIZE (1024)
+
+typedef struct Payload
+{
+    float temperature;
+    float pressure;
+    float altitude;
+} Payload;
+
+Payload payload;
+
+static void setup_uart()
+{
+    /* Configure parameters of an UART driver,
+     * communication pins and install the driver */
+    uart_config_t uart_config = {
+            .baud_rate = ECHO_UART_BAUD_RATE,
+            .data_bits = UART_DATA_8_BITS,
+            .parity    = UART_PARITY_DISABLE,
+            .stop_bits = UART_STOP_BITS_1,
+            .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+            .source_clk = UART_SCLK_APB,
+    };
+    int intr_alloc_flags = 0;
+
+#if CONFIG_UART_ISR_IN_IRAM
+    intr_alloc_flags = ESP_INTR_FLAG_IRAM;
+#endif
+
+    ESP_ERROR_CHECK(uart_driver_install(ECHO_UART_PORT_NUM, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
+    ESP_ERROR_CHECK(uart_param_config(ECHO_UART_PORT_NUM, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(ECHO_UART_PORT_NUM, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS, ECHO_TEST_CTS));
+}
+
+static void listen_uart()
+{
+    while (1) {
+        // Read data from the UART
+        int len = uart_read_bytes(ECHO_UART_PORT_NUM, &payload, sizeof(Payload), 20 / portTICK_RATE_MS);
+        printf(".");
+        if (len) {
+            printf("\n\nReceived %d bytes\n", len);
+            printf("\nTemperature: %.2f °C", payload.temperature);
+            printf("\nPressure: %.2f Pa", payload.pressure);
+            printf("\nAltitude: %.2f m", payload.altitude);
+            printf("\n\n");
+        }
+    }
+}
+
+static void uart_task(void *arg)
+{
+    setup_uart();
+    listen_uart();
+}
+
 static void message_handler(coap_context_t *ctx, coap_session_t *session,
                             coap_pdu_t *sent, coap_pdu_t *received,
                             const coap_tid_t id)
@@ -439,15 +519,17 @@ clean_up:
 
 void app_main(void)
 {
-    ESP_ERROR_CHECK( nvs_flash_init() );
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+//    ESP_ERROR_CHECK( nvs_flash_init() );
+//    ESP_ERROR_CHECK(esp_netif_init());
+//    ESP_ERROR_CHECK(esp_event_loop_create_default());
+//
+//    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+//     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+//     * examples/protocols/README.md for more information about this function.
+//     */
+//    ESP_ERROR_CHECK(example_connect());
+//
+//    xTaskCreate(coap_example_client, "coap", 8 * 1024, NULL, 5, NULL);
 
-    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
-    ESP_ERROR_CHECK(example_connect());
-
-    xTaskCreate(coap_example_client, "coap", 8 * 1024, NULL, 5, NULL);
+    xTaskCreate(uart_task, "uart_echo_task", ECHO_TASK_STACK_SIZE, NULL, 10, NULL);
 }
